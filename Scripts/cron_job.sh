09f0414cb6c6ea7604fd7c47f080f7d7a93529ea
sagarbangera <sagarbangera@gmail.com>
2017-09-19 09:04:04 +0530

    Script files

--- Scripts/cron_job.sh
+++ Scripts/cron_job.sh
@@ -8,8 +8,10 @@
 export AWS_ACCESS_KEY_ID=jgfabjkfajgankfkfasbkfa
 export AWS_SECRET_ACCESS_KEY=bfgofbkfbvhoslgjpjgnlbkhpjsnhonslnl
 
-mv /tmp/mysql_backup.sql /tmp/mysql_backup-`date +%d%b%y`.sql
+mv /tmp/docker_mysql_backup.sql /tmp/mysql_backup-`date +%d%b%y`.sql
 mv /tmp/docker_logfiles.tar.gz /tmp/docker_logfiles-`date +%d%b%y`.tar.gz
+mv /tmp/alldb.sql /tmp/alldb-`date +%d%b%y`.sql
 
-aws s3 cp /tmp/mysql_backup-`date +%d%b%y`.sql s3://sagar-backup/backup/
-aws s3 cp /tmp/docker_logfiles-`date +%d%b%y`.tar.gz s3://sagar-backup/logs/
+aws s3 cp /tmp/mysql_backup-`date +%d%b%y`.sql s3://sagarbangera/backup/
+aws s3 cp /tmp/alldb-`date +%d%b%y`.sql s3://sagarbangera/backup/
+aws s3 cp /tmp/docker_logfiles-`date +%d%b%y`.tar.gz s3://sagarbangera/logs/
--- Scripts/make_backup.sh
+++ Scripts/make_backup.sh
@@ -6,4 +6,6 @@ BKP_PATH="/tmp"
 rm -rf $BKP_PATH/mysql.sql
 
 # Make a New Backup
-docker run --rm --link sagar-mysql:sagar-mysql -v $BKP_PATH:/backup mysql sh -c 'mysqldump -uroot -psagar -P3306 -hsagar-mysql mysql > backup/mysql_backup.sql'
+docker run --rm --link sagar-mysql:sagar-mysql -v $BKP_PATH:/backup mysql sh -c 'mysqldump -uroot -psagar -P3306 -hsagar-mysql mysql > backup/docker_mysql_backup.sql'
+
+mysqldump -u root -psagar --all-databases > "$BKP_PATH"/alldb.sql
